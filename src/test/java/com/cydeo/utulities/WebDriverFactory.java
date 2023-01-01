bc24f3d5812d6b5870a1fe642ca980e53d6e1cf5
Tugba Borekci <tugbaborekci80@gmail.com>
2023-01-01 21:42:08 +0000

    WebDriverFactory added

--- src/test/java/com/cydeo/utulities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utulities/WebDriverFactory.java
@@ -11,24 +11,26 @@ package com.cydeo.utulities;
 //RETURN TYPE: "WebDriver
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
 public class WebDriverFactory {
 
-    public static WebDriver getDriver(@NotNull String browserType){ //static method kullandik. RETURN TYPE: "WebDriver--> Method name: getDriver(String Browser type)
-if (browserType.equalsIgnoreCase("chrome")){
-    WebDriverManager.chromedriver().setup();
-    return new ChromeDriver();
-} else if (browserType.equalsIgnoreCase("firefox")) {
-    WebDriverManager.firefoxdriver().setup();
-    return new FirefoxDriver();
-}else{
-    System.out.println("Given browser type does not exist/or is not currently supported");
-    System.out.println("Driver = null");
-    return null;//it will be empty situation
+    public static WebDriver getDriver(String browserType){ //static method kullandik. RETURN TYPE: "WebDriver--> Method name: getDriver(String Browser type)
+        if (browserType.equalsIgnoreCase("chrome")){
+
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        }else if (browserType.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else{
+            System.out.println("Given browser type does not exist/or is not currently supported");
+            System.out.println("Driver = null");
+            return null;//it will be empty situation
 }
 
     }
