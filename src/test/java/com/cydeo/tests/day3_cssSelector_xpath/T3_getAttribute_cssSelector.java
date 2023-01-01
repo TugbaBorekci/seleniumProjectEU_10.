7c30e566eb8af35e5886e0c8db7b34c3b156dd88
Tugba Borekci <tugbaborekci80@gmail.com>
2023-01-01 21:44:10 +0000

    committed

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 
 public class T3_getAttribute_cssSelector {
     public static void main(String[] args) {
-
         //TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
         //1- Open a Chrome browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
@@ -18,29 +17,29 @@ public class T3_getAttribute_cssSelector {
 
         //3- Verify “Log in” button text is as expected:
         //Expected: Log In
+        //WebElement signInButton = driver.findElement(By.className("login-btn"));
 
         //LOCATING THE SAME WEB ELEMENT USING DIFFERENT ATTRIBUTE VALUES.
-        //                      tagName[attribute='value]
-        //                      input[class='login-btn']
-        //LOCATED USING CLASS ATTRIBUTE
+        //	                   tagName[attribute='value']
+        //	                   input[class='login-btn']
+        // LOCATED USING CLASS ATTRIBUTE
         //WebElement signInButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
-        //LOCATED USING TYPE ATTRIBUTE
+        // LOCATED USING TYPE ATTRIBUTE
         //WebElement signInButton = driver.findElement(By.cssSelector("input[type='submit']"));
-        //LOCATED USING VALUE ATTRIBUTE
-        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In]"));
+        // LOCATED USING VALUE ATTRIBUTE
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In']"));
 
-        String expectedButton ="Log In";
+        String expectedButtonText = "Log In";
 
         //Getting the value of the attribute "value"
         String actualButtonText = signInButton.getAttribute("value");
 
         System.out.println("actualButtonText = " + actualButtonText);
 
-        if (actualButtonText.equals(expectedButton)){
-            System.out.println("Log In the button text verification is PASSED!");
-        }else {
-            System.out.println("Log In the button text verification is PASSED!");
+        if (actualButtonText.equals(expectedButtonText)){
+            System.out.println("Log In button text verification passed!");
+        }else{
+            System.out.println("Log In button text verification failed!");
         }
-
     }
 }
