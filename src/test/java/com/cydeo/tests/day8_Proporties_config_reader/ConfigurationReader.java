3b80414282cd879e5618c370a29dee3635719d99
Tugba Borekci <tugbaborekci80@gmail.com>
2023-01-17 19:51:45 +0000

    added

--- src/test/java/com/cydeo/tests/day8_Proporties_config_reader/ConfigurationReader.java
+++
@@ -1,6 +0,0 @@
-package com.cydeo.tests.day8_Proporties_config_reader;
-
-public class ConfigurationReader {
-
-
-}
--- src/test/java/com/cydeo/utulities/ConfigurationReader.java
+++ src/test/java/com/cydeo/utulities/ConfigurationReader.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class ConfigurationReader {
 
     //1- Create the object of Properties
-   private static Properties properties = new Properties();
+   public static Properties properties = new Properties();
 
    static {
 
